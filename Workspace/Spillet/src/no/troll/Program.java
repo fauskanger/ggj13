10698aa33c7d2766f9abc068e05991db135b566f
Stian Fauskanger <stianfauskanger@gmail.com>
2013-01-26 00:19:18 +0100

    Removed error files

--- Workspace/Spillet/src/no/troll/Program.java
+++ Workspace/Spillet/src/no/troll/Program.java
@@ -1,13 +1,44 @@
 package no.troll;
 
-public class Program {
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Program extends BasicGame {
+
+	public Program() {
+		super("Hello World");
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer app = new AppGameContainer(new Program());
+		 
+	     app.setDisplayMode(800, 600, false);
+	     app.start();
+	}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException {
 		// TODO Auto-generated method stub
-		TestKlasse tk = new TestKlasse();
+		g.drawString("Hello World", 100, 100);
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer gc, int delta) throws SlickException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
