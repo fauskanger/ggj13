a759e68280dfe2e95841625c02db3286f1156cc6
Guchoo <orug_oedesnel@hotmail.com>
2013-01-26 03:00:20 +0100

    KeyInput

--- Workspace/Spillet/src/no/troll/Program.java
+++ Workspace/Spillet/src/no/troll/Program.java
@@ -4,19 +4,24 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class Program extends BasicGame {
+	
+	Image testImg;
+	int X = 200;
+	int Y = 200;
 
-	public Program() {
+	public Program() 
+	{
 		super("Hello World");
-		// TODO Auto-generated constructor stub
+		
 	}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws SlickException {
+	public static void main(String[] args) throws SlickException 
+	{
 		AppGameContainer app = new AppGameContainer(new Program());
 		 
 	     app.setDisplayMode(800, 600, false);
@@ -27,18 +32,24 @@ public class Program extends BasicGame {
 	public void render(GameContainer gc, Graphics g) throws SlickException {
 		// TODO Auto-generated method stub
 		g.drawString("Hello World", 100, 100);
+		g.drawImage(testImg,X,Y);
 	}
 
 	@Override
 	public void init(GameContainer gc) throws SlickException {
-		// TODO Auto-generated method stub
-		
+		testImg=new Image("C:\\Users\\Guchoo\\Pictures\\Ipod\\Noodle-gorillaz.jpg");
+				
 	}
 
 	@Override
 	public void update(GameContainer gc, int delta) throws SlickException {
-		// TODO Auto-generated method stub
 		
+		Input input = gc.getInput();
+		if(input.isKeyDown(Input.KEY_UP)){Y -=1;}
+		if(input.isKeyDown(Input.KEY_DOWN)){Y ++;}
+		if(input.isKeyDown(Input.KEY_LEFT)){X -=1;}
+		if(input.isKeyDown(Input.KEY_RIGHT)){X ++;}
+
 	}
 
 }
