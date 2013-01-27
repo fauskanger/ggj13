7995eb6a290e2b0e3bc7352616ad740bf066e254
Thomas Fauskanger <thomasfauskanger@gmail.com>
2013-01-27 05:08:42 +0100

    Added Troll

---
+++ Workspace/Spillet/src/no/troll/Troll.java
@@ -0,0 +1,498 @@
+package no.troll;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import no.troll.Resources.CharacterImageName;
+import no.troll.Resources.SoundName;
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.Sound;
+import org.newdawn.slick.geom.Polygon;
+import org.newdawn.slick.geom.Transform;
+
+
+public class Troll implements Drawable {
+
+	private enum MoveDirection {UP, DOWN, LEFT, RIGHT, UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT, STILL};
+
+	private int posX;
+	private int posY;
+	private double delta_posX;
+	private double delta_posY; 
+
+	private ArrayList<Brick> fixedObjects;
+
+	private HashMap<MoveDirection, Image[]> images;	
+	private Resources resources;
+
+	private boolean attack = false; 
+	private SoundManager Lyd; 
+
+	private Brick lastCollisionBrick;
+
+
+	private int timePerSpriteLoop; // in msec
+	private int deltaTimeSpriteLoop; // in msec
+
+	private int freeZoneTop;
+	private int freeZoneRight;
+	private int freeZoneBottom;
+	private int freeZoneLeft;
+
+	private Polygon shape;
+	private Virgin virgin;
+
+	private MoveDirection currentMoveDirection;
+	private int currentSpriteFrame;
+
+	public Troll(Resources resources, int posX, int posY, Virgin virgin, ArrayList<Brick> fixedObjects) {
+		this.virgin = virgin;
+		this.resources = resources;
+		this.fixedObjects = fixedObjects;
+		this.images = new HashMap<Troll.MoveDirection, Image[]>();
+		this.posX = posX;
+		this.posY = posY;
+		delta_posX = 0;
+		delta_posY = 0;
+		currentMoveDirection = MoveDirection.STILL;
+		currentSpriteFrame = 0;
+		timePerSpriteLoop = 500;
+		deltaTimeSpriteLoop = 0;
+
+		addImages();
+
+		int bottomY = posY + images.get(MoveDirection.STILL)[0].getHeight();
+		int topY = bottomY - Program.tileHeight*2;
+		int leftY = bottomY - Program.tileHeight;
+		int rightY = leftY;
+
+		int bottomX = posX + Program.tileWidth;
+		int leftX = posX;
+		int rightX = posX + Program.tileWidth*2;
+		int topX = bottomX;
+		shape = new Polygon();
+		shape.addPoint(bottomX, bottomY);
+		shape.addPoint(rightX, rightY);
+		shape.addPoint(topX, topY);
+		shape.addPoint(leftX, leftY);
+	}
+
+	private void addImages() {
+		Image[] arr = new Image[3];
+		arr[0] = resources.getCharacter(CharacterImageName.Troll_UP_1);
+		arr[1] = resources.getCharacter(CharacterImageName.Troll_UP_2);
+		arr[2] = resources.getCharacter(CharacterImageName.Troll_UP_3);
+		images.put(MoveDirection.UP, arr);
+
+		arr = new Image[3];
+		arr[0] = resources.getCharacter(CharacterImageName.Troll_DOWN_1);
+		arr[1] = resources.getCharacter(CharacterImageName.Troll_DOWN_2);
+		arr[2] = resources.getCharacter(CharacterImageName.Troll_DOWN_3);
+		images.put(MoveDirection.DOWN, arr);
+
+		arr = new Image[3];
+		arr[0] = resources.getCharacter(CharacterImageName.Troll_LEFT_1);
+		arr[1] = resources.getCharacter(CharacterImageName.Troll_LEFT_2);
+		arr[2] = resources.getCharacter(CharacterImageName.Troll_LEFT_3);
+		images.put(MoveDirection.LEFT, arr);
+
+		arr = new Image[3];
+		arr[0] = resources.getCharacter(CharacterImageName.Troll_RIGHT_1);
+		arr[1] = resources.getCharacter(CharacterImageName.Troll_RIGHT_2);
+		arr[2] = resources.getCharacter(CharacterImageName.Troll_RIGHT_3);
+		images.put(MoveDirection.RIGHT, arr);
+
+		arr = new Image[3];
+		arr[0] = resources.getCharacter(CharacterImageName.Troll_UPLEFT_1);
+		arr[1] = resources.getCharacter(CharacterImageName.Troll_UPLEFT_2);
+		arr[2] = resources.getCharacter(CharacterImageName.Troll_UPLEFT_3);
+		images.put(MoveDirection.UPLEFT, arr);
+
+		arr = new Image[3];
+		arr[0] = resources.getCharacter(CharacterImageName.Troll_UPRIGHT_1);
+		arr[1] = resources.getCharacter(CharacterImageName.Troll_UPRIGHT_2);
+		arr[2] = resources.getCharacter(CharacterImageName.Troll_UPRIGHT_3);
+		images.put(MoveDirection.UPRIGHT, arr);
+
+		arr = new Image[3];
+		arr[0] = resources.getCharacter(CharacterImageName.Troll_DOWNLEFT_1);
+		arr[1] = resources.getCharacter(CharacterImageName.Troll_DOWNLEFT_2);
+		arr[2] = resources.getCharacter(CharacterImageName.Troll_DOWNLEFT_3);
+		images.put(MoveDirection.DOWNLEFT, arr);
+
+		arr = new Image[3];
+		arr[0] = resources.getCharacter(CharacterImageName.Troll_DOWNRIGHT_1);
+		arr[1] = resources.getCharacter(CharacterImageName.Troll_DOWNRIGHT_2);
+		arr[2] = resources.getCharacter(CharacterImageName.Troll_DOWNRIGHT_3);
+		images.put(MoveDirection.DOWNRIGHT, arr);
+
+		arr = new Image[1];
+		arr[0] = resources.getCharacter(CharacterImageName.Troll_STILL);
+		images.put(MoveDirection.STILL, arr);
+	}
+
+	public void draw(Graphics g) {
+
+		//		g.drawImage(image, getScreenPos().x, getScreenPos().y);
+
+
+		drawTileLines(g);
+		//		System.out.println("Koza: " + currentMoveDirection + " " + currentSpriteFrame);
+
+		Image i = images.get(currentMoveDirection)[currentSpriteFrame];
+		g.drawImage(i, posX, posY);
+
+		//g.drawImage(image, posX, posY);
+	}
+
+	private void drawTileLines(Graphics g) {
+		g.draw(shape);
+	}
+
+	@Override
+	public void updatePosition(int delta_x, int delta_y) {
+		this.posX += delta_x;
+		this.posY += delta_y;
+		translatePolygon(delta_x, delta_y);
+	}
+
+	@Override
+	public void update(int delta, Pair delta_pos) {
+
+		int pixels_per_sec = 100;
+		double time = (double)delta / 1000;
+		double pixels = time * pixels_per_sec;
+
+		boolean moveUp = (virgin.getZ().y < getZ().y);
+		boolean moveDown = !moveUp;
+		boolean moveLeft = (virgin.getZ().x < getZ().x);
+		boolean moveRight = !moveLeft;
+		
+		currentMoveDirection = getCurrentMoveDirection(moveUp, moveDown, moveLeft, moveRight);
+		int numberOfSpriteElements = images.get(currentMoveDirection).length;
+		deltaTimeSpriteLoop += delta;
+		if (deltaTimeSpriteLoop > timePerSpriteLoop / numberOfSpriteElements) {
+			currentSpriteFrame = (currentSpriteFrame + 1);
+			deltaTimeSpriteLoop -= timePerSpriteLoop / numberOfSpriteElements;
+		}
+		currentSpriteFrame %= numberOfSpriteElements;
+		
+		if (moveDown) {
+			delta_posX -= pixels * 2;
+			delta_posY += pixels;
+		}
+		if (moveUp) {
+			delta_posX += pixels * 2;
+			delta_posY -= pixels;
+		}
+		if (moveLeft) {
+			delta_posX -= pixels * 2;
+			delta_posY -= pixels;
+		}
+		if (moveRight) {
+			delta_posX += pixels * 2;
+			delta_posY += pixels;
+		}
+		int moveX = (int) delta_posX;
+		int moveY = (int) delta_posY;
+		delta_posX -= moveX;
+		delta_posY -= moveY;
+		
+		updatePosition(delta_pos.x, delta_pos.y); 	// Compensate for screen movement
+		updatePosition(moveX, moveY); 				// Chase Gro the Virgin
+	}
+
+
+	//	private Pair getScreenPos() {
+	//	    int retX = posX * (Program.tileWidth / 2) + posY * -Program.tileHeight;
+	//	    int retY = posX * (Program.tileHeight / 2) + posY * (Program.tileHeight / 2); 		
+	//		return new Pair(retX, retY);
+	//		
+	//	}
+
+	public void attack(Input input)
+	{		
+		if(input.isKeyDown(Input.KEY_X))
+		{
+			attack = true;
+
+			Sound att = resources.getSound(SoundName.Sword); 
+			if(!att.playing())
+			{
+				att.play(1.0f,0.2f);
+			}
+			//Bilde av angrep
+
+		}
+	}
+
+	public Pair update(int delta, Input input) {
+		int pixels_per_sec = 150;
+		double time = (double)delta / 1000;
+		double pixels = time * pixels_per_sec;
+
+		int buttons = 0;
+		boolean moveUp = false;
+		boolean moveDown = false;
+		boolean moveLeft = false;
+		boolean moveRight = false;
+
+		attack(input);
+
+		if (input.isKeyDown(Input.KEY_UP) && input.isKeyDown(Input.KEY_LEFT)) {
+			moveLeft = true;
+			buttons = 1;
+		}
+		else if (input.isKeyDown(Input.KEY_DOWN) && input.isKeyDown(Input.KEY_RIGHT)) {
+			moveRight = true;
+			buttons = 1;
+		}
+		else if (input.isKeyDown(Input.KEY_DOWN) && input.isKeyDown(Input.KEY_LEFT)) {
+			moveDown = true;
+			buttons = 1;
+		}
+		else if (input.isKeyDown(Input.KEY_UP) && input.isKeyDown(Input.KEY_RIGHT)) {
+			moveUp = true;
+			buttons = 1;
+		}
+
+		else if (input.isKeyDown(Input.KEY_UP)) {
+			moveUp = true;
+			moveLeft = true;
+			buttons = 2;
+		}
+		else if (input.isKeyDown(Input.KEY_DOWN)) {
+			moveDown = true;
+			moveRight = true;
+			buttons = 2;
+		}
+		else if (input.isKeyDown(Input.KEY_LEFT)) {
+			moveLeft = true;
+			moveDown = true;
+			buttons = 2;
+		}
+		else if (input.isKeyDown(Input.KEY_RIGHT)) {
+			moveRight = true;
+			moveUp = true;
+			buttons = 2;
+		}
+
+		currentMoveDirection = getCurrentMoveDirection(moveUp, moveDown, moveLeft, moveRight);
+		int numberOfSpriteElements = images.get(currentMoveDirection).length;
+		deltaTimeSpriteLoop += delta;
+		if (deltaTimeSpriteLoop > timePerSpriteLoop / numberOfSpriteElements) {
+			currentSpriteFrame = (currentSpriteFrame + 1);
+			deltaTimeSpriteLoop -= timePerSpriteLoop / numberOfSpriteElements;
+		}
+		currentSpriteFrame %= numberOfSpriteElements;
+
+		if (buttons > 1) {
+			if (moveDown == moveLeft) { //Moving left/right
+				pixels *= 0.6;
+			}
+		}
+
+		if (moveDown) {
+			delta_posX -= pixels * 2;
+			delta_posY += pixels;
+		}
+		if (moveUp) {
+			delta_posX += pixels * 2;
+			delta_posY -= pixels;
+		}
+		if (moveLeft) {
+			delta_posX -= pixels * 2;
+			delta_posY -= pixels;
+		}
+		if (moveRight) {
+			delta_posX += pixels * 2;
+			delta_posY += pixels;
+		}
+		int moveX = (int) delta_posX;
+		int moveY = (int) delta_posY;
+		delta_posX -= moveX;
+		delta_posY -= moveY;
+
+		updatePosition(moveX, moveY);
+
+
+		int correctX = 0;
+		int correctY = 0;
+
+		if (!collisionDetection(currentMoveDirection)) {			
+
+			if (posY < freeZoneTop) {
+				correctY = freeZoneTop - posY;
+			}
+			if (posY > freeZoneBottom) {
+				correctY = freeZoneBottom - posY;
+			}
+			if (posX < freeZoneLeft) {
+				correctX = freeZoneLeft - posX;
+			}
+			if (posX > freeZoneRight) {
+				correctX = freeZoneRight - posX;
+			}
+
+			updatePosition(correctX, correctY);
+		}
+		else
+		{
+			int dy,dx;
+			boolean isAboveMiddle, isRight;
+			boolean randomBool = (Math.random()>=0.5);
+			
+			boolean previousIsAboveMiddle;
+			boolean previousIsRight;
+			
+
+			previousIsRight = isRight = (getZ().x > lastCollisionBrick.getZ().x);
+			previousIsAboveMiddle = isAboveMiddle = (getZ().y < lastCollisionBrick.getZ().y);
+			
+			while(collisionDetection(currentMoveDirection)) {
+				
+				isRight = (getZ().x > lastCollisionBrick.getZ().x);
+				isAboveMiddle = (getZ().y < lastCollisionBrick.getZ().y);
+				
+				if (isRight!=previousIsRight || isAboveMiddle!=previousIsAboveMiddle)
+					break;
+				
+				if (isRight)
+				{
+					switch (currentMoveDirection)
+					{
+					case UP:
+						dx = 1;
+						dy = 1;
+						break;
+					case DOWN:
+						dx = 1;
+						dy = -1;
+						break;
+					case DOWNLEFT:
+						dx = 1; 
+						dy = -1;
+						break;
+					case UPLEFT:
+						dx = 1; 
+						dy = 1;
+						break;
+					case LEFT:
+						if(isAboveMiddle) {
+							dx = 1;
+							dy = -1;
+						} else {
+							dx = 1;
+							dy = 1;
+						}
+						break;
+					default:
+						dx = moveX>0? 1:-1;
+						dy = moveY>0? 1:-1;
+						System.out.println("ERROR! TROLL Switch resulted in default on isRight collision " +  currentMoveDirection);
+//						System.exit(0);
+						break;
+					}
+				}
+				else // !isRight => isLeft
+				{
+					switch (currentMoveDirection)
+					{
+					case UP:
+						dx = -1;
+						dy = 1;
+						break;
+					case DOWN:
+						dx = -1;
+						dy = -1;
+						break;
+					case RIGHT:
+						if (isAboveMiddle) {
+							dx = -1;
+							dy = -1;
+						} else {
+							dx = -1;
+							dy = 1;
+						}
+						break;
+					case UPRIGHT:
+						dx = -1;
+						dy = 1;
+						break;
+					case DOWNRIGHT:
+						dx = -1;
+						dy = -1;
+						break;
+					default:
+						dx = moveX>0? 1:-1;
+						dy = moveY>0? 1:-1;
+						System.out.println("ERROR! TROLL switch resulted in default on isLeft collision " + currentMoveDirection);
+//						System.exit(0);
+						break;
+					}
+				}
+
+				updatePosition(dx, dy);
+				previousIsAboveMiddle = isAboveMiddle;
+				previousIsRight = isRight;
+			}	
+		}
+
+		return new Pair(correctX, correctY);
+	}
+
+	private void translatePolygon(int moveX, int moveY) {
+		shape = (Polygon) shape.transform(Transform.createTranslateTransform(moveX, moveY));
+	}
+
+	private MoveDirection getCurrentMoveDirection(boolean moveUp, boolean moveDown,	boolean moveLeft, boolean moveRight) {
+		if (moveUp && moveLeft) {
+			return MoveDirection.UP;
+		}
+		else if (moveDown && moveRight) {
+			return MoveDirection.DOWN;
+		}
+		else if (moveDown && moveLeft) {
+			return MoveDirection.LEFT;
+		}
+		else if (moveUp && moveRight) {
+			return MoveDirection.RIGHT;
+		}
+		else if (moveUp) {
+			return MoveDirection.UPRIGHT;
+		}
+		else if (moveRight) {
+			return MoveDirection.DOWNRIGHT;
+		}
+		else if (moveDown) {
+			return MoveDirection.DOWNLEFT;
+		}
+		else if (moveLeft) {
+			return MoveDirection.UPLEFT;
+		}
+		return MoveDirection.STILL;
+	}
+
+	@Override
+	public Pair getZ() {
+		return new Pair(posX+Program.tileWidth/2, posY + images.get(MoveDirection.STILL)[0].getHeight());
+	}
+
+	boolean collisionDetection(MoveDirection moveDirection) {
+		for(Brick b: fixedObjects) {
+			if(b.shape.intersects(shape)) {
+				lastCollisionBrick = b;
+				System.out.println("TROLL COLLISION ON BRICK");
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+
+
+}
